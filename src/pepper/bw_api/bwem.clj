b21052e8733bf3c0cd4113213e422e08f69c5010
YesNoMaybeItDepends <22201184+YesNoMaybeItDepends@users.noreply.github.com>
2025-05-10 12:12:31 +0200

    add bwem ns

---
+++ src/pepper/bw_api/bwem.clj
@@ -0,0 +1,26 @@
+(ns pepper.bw-api.bwem
+  (:require [pepper.bw-api.game :as game])
+  (:import (bwem BWEM BWMap)))
+
+(defonce bwem nil)
+(defn bind-bwem! [b] (alter-var-root #'bwem (constantly b)))
+(defn init []
+  (let [bwem (new BWEM game/game)]
+    (.setFailOnError bwem false)
+    (.initialize bwem)
+    (.setFailOnError bwem true)
+    bwem))
+
+(defn get-map
+  []
+  (.getMap bwem))
+
+(defn get-starting-locations
+  []
+  (.getStartingLocations (get-map)))
+
+(defn get-unassigned-starting-locations []
+  (.getUnassignedStartingLocations (get-map)))
+
+(defn get-bases [])
+
