4bb3ce324984e20b333ae7be6d8c3c0f5b11fdba
YesNoMaybeItDepends <22201184+YesNoMaybeItDepends@users.noreply.github.com>
2025-05-11 14:52:27 +0200

    add interop utils

---
+++ src/pepper/utils/interop.clj
@@ -0,0 +1,43 @@
+(ns pepper.utils.interop
+  (:require [clojure.reflect :as reflect]
+            [clojure.repl :as repl]
+            [clojure.java.data :as j]
+            [clojure.string :as str]
+            [lambdaisland.deep-diff2 :as ddiff]
+            [editscript.core :as e]))
+
+(defn- kebabify-property
+  [string]
+  (let [regex #"(?<![A-Z])[A-Z](?![A-Z])|(?<=[^A-Z:])[A-Z](?=[A-Z])|(?<=[A-Z])[A-Z](?![A-Z])(?=[a-z])"]
+    (str/replace string regex #(str "-" %1))))
+
+(defn- property->keyword
+  [property]
+  {:property property
+   :keyword (-> property
+                name
+                kebabify-property
+                str/lower-case
+                keyword)})
+
+(defn- obj->bean [obj]
+  (j/from-java-shallow obj {}))
+
+(defn- bean->map
+  [bean]
+  (update-keys bean (:keyword property->keyword)))
+
+(defn- bean->index
+  [bean]
+  (map property->keyword (keys bean)))
+
+(defn obj->index
+  [obj]
+  (->> obj
+       obj->bean
+       bean->index))
+
+(defn obj->map
+  [obj]
+  (-> obj->bean
+      bean->map))
