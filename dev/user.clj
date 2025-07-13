912e4bebf7ce0d1538a18996d6b7926767cc9e96
YesNoMaybeItDepends <22201184+YesNoMaybeItDepends@users.noreply.github.com>
2025-07-13 11:04:48 +0200

    update deps.edn and user ns

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,5 @@
 (ns user
   (:require
-   [pepper.systems.portal :as portal]
    [clojure.reflect :as reflect]
    [clojure.core.async :as a]
    [clojure.core.async.flow :as flow]
@@ -17,7 +16,7 @@
    [taoensso.telemere :as t]
    [pepper.utils.chaoslauncher :refer [stop!]]
    #_[clojure.spec.test.alpha :as st]
-   [portal.api :as p]
+   [portal.api :as portal]
    [clojure.tools.namespace.find :as ns-find]
    [clojure.java.classpath :as cp]
    [clojure.tools.namespace.parse :as ns-parse]
@@ -34,40 +33,97 @@
    
    IMPORTANT: Can't eval automatically during Calva jack-in?
 
-   - [ ] TODO: fix that so I don't have to eval this manually"
+   - [ ] TODO: fix that so I don't have to eval this manually
+   - [ ] TODO: Why did I put this inside its own function?"
   []
   (set! *print-namespace-maps* false))
+
+;; Would be better if it was a function that returned the config or something like that
+;; So that it would be changeable
+(def system-config
+  {:config/portal {:config/auto-init? true
+                   :config/is-init? :portal/instance
+                   :config/init-fn (fn [state]
+                                     (add-tap #'portal/submit)
+                                     (assoc state :portal/instance (portal/open {:theme :portal.colors/nord})))
+                   :config/stop-fn (fn [state]
+                                     (remove-tap #'portal/submit)
+                                     (portal/close (:portal/instance state))
+                                     (portal/stop)
+                                     (assoc state :portal/instance nil))}
+   :config/zprint {:config/auto-init? true
+                   :config/is-init? :zprint/tapped
+                   :config/init-fn (fn [state]
+                                     (add-tap #'zp/zprint)
+                                     (assoc state :zprint/tapped true))
+                   :config/stop-fn (fn [state]
+                                     (remove-tap #'zp/zprint)
+                                     (assoc state :zprint/tapped false))}
+   ;; Instrumentation isn't a system but I'll rewrite this at some other time
+   :config/instrumentation {:config/auto-init? true
+                            :config/is-init? :instrumentation/instrumented
+                            :config/init-fn (fn [state]
+                                              (specs/instrument)
+                                              state)
+                            :config/stop-fn (fn [state]
+                                              state)}})
+
+;; another config idea
+#_{:config {:config/id :portal
+            :config/should-init? (fn [state config] (not ((:config/id config) state)))
+            :config/init-fn (fn [] :val)}
+   :state {}
+   :init-fn (fn [state some-config]
+              (if ((:config/should-init? some-config) state some-config)
+                (assoc state (:config/id some-config) (:config/init-fn some-config))
+                state))}
+
+(defn init-config! [state {:config/keys [auto-init?
+                                         is-init?
+                                         init-fn]}]
+  (if (and (true? auto-init?)
+           (ifn? is-init?)
+           (not (is-init? state)))
+    (init-fn state)
+    state))
+
+(defn stop-config! [state {:config/keys [is-init?
+                                         stop-fn]}]
+  (if (and (ifn? is-init?)
+           (is-init? state))
+    (stop-fn state)
+    state))
+
+(defn init-system [config]
+  (let [config config
+        store (atom {})]
+
+    (fn
+      ([] {:atom store
+           :config config})
+      ([action]
+       (let [action-fn (case action
+                         :init #'init-config!
+                         :stop #'stop-config!
+                         (fn [state _] state))
+             state (reduce (fn [state [entry entry-config]]
+                             (action-fn state entry-config))
+                           @store
+                           config)]
+         (swap! store merge state))))))
+
+(defonce system (init-system system-config))
+(system :stop)
+(system :init)
 
 (comment
 
   (disable-namespace-maps)
 
+  (try (pepper/-main)
+       (catch Exception e (println e)))
+
   #_())
-
-(defn init? [k]
-  (k {:portal true
-      :flowstorm false
-      :pepper false
-      :instrument true
-      :zprint true
-      :tests true}))
-
-(def portal (when (init? :portal)
-              (atom (portal/start!))))
-
-(def flowstorm (when (init? :flowstorm)
-                 (fs-api/local-connect)
-                 true))
-
-(when (init? :zprint)
-  (add-tap zp/zprint))
-
-(when (init? :instrument)
-  (specs/instrument))
-
-(comment (try (pepper/-main)
-              (catch Exception e (println e)))
-         #_())
 
 (comment "Trying to load all test namespaces"
 
