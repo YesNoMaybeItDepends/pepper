d707e2cd67d741c6fffe6f9691f34a2f164e2d02
YesNoMaybeItDepends <22201184+YesNoMaybeItDepends@users.noreply.github.com>
2025-08-20 18:48:08 +0200

    military (scouting)

--- src/pepper/game/military.clj
+++ src/pepper/game/military.clj
@@ -26,15 +26,15 @@
   (or (:empty-starting-bases state)
       []))
 
-(defn our-starting-base [state]
+(defn fetch-our-starting-base [state]
   (player/starting-base (player/get-self state)))
 
-(defn all-starting-bases [state]
+(defn fetch-all-starting-bases [state]
   (map/starting-bases (map/map state)))
 
 (defn possible-enemy-starting-bases [state]
-  (let [all-starting-bases (all-starting-bases state)
-        our-starting-base (our-starting-base state)
+  (let [all-starting-bases (fetch-all-starting-bases state)
+        our-starting-base (fetch-our-starting-base state)
         empty-starting-bases (empty-starting-bases state)
         not-enemy-starting-bases (into #{} (conj empty-starting-bases our-starting-base))]
     (filterv (complement not-enemy-starting-bases) all-starting-bases)))
@@ -45,17 +45,35 @@
 (defn know-enemy-starting-base? [state]
   (some? (enemy-starting-base state)))
 
+(defn add-unit-finding-enemy-starting-base [military unit-id]
+  (update military :units-finding-enemy-starting-base conj unit-id))
+
+(defn update-military [state military]
+  (update state :military merge military))
+
+(defn military [state]
+  (:military state))
+
+(defn init-military [state]
+  {:military {:our-starting-base (fetch-our-starting-base state)
+              :enemy-starting-base nil
+              :possible-enemy-starting-bases (filterv (complement #{fetch-our-starting-base}) (fetch-all-starting-bases state))
+              :discarded-enemy-starting-bases []
+              :units-finding-enemy-starting-base #{}}}) ;; unit -> base tuple, eg [3 [7 113]]
+
 (defn assign-scouting-job [state worker-id]
   (let [possible-enemy-starting-bases (possible-enemy-starting-bases state)
         starting-bases-already-being-scouted (starting-bases-already-being-scouted state)
         starting-base-to-scout (first possible-enemy-starting-bases) ;; TODO: filter starting-bases-already-being-scouted !!!!!!!
         job (find-enemy-starting-base/job starting-base-to-scout worker-id)] ;; TODO: if some base to scout then add job and stuff, otherwise skip adding it bro
-    (jobs/assign-unit-job state job)))
+    (-> state
+        (update :military (fn [m] (add-unit-finding-enemy-starting-base m worker-id)))
+        (jobs/assign-unit-job job))))
 
-(defn maybe-scout [state]
-  (let [already-scouting? (already-scouting? state)
+(defn maybe-find-enemy-starting-base [state]
+  (let [know-enemy-starting-base? (know-enemy-starting-base? state)
+        already-scouting? (already-scouting? state)
         barracks-completed? (barracks-completed? state)
-        know-enemy-starting-base? (know-enemy-starting-base? state)
         some-available-worker (unit/get-idle-or-mining-worker state)]
     (if (and (not know-enemy-starting-base?)
              barracks-completed?
@@ -64,13 +82,6 @@
       (assign-scouting-job state (unit/id some-available-worker))
       state)))
 
-;;     {:all-starting-bases all-starting-bases
-;;      :our-starting-base our-starting-base
-;;      :possible-enemy-starting-bases (filterv (complement #{our-starting-base}) all-starting-bases)
-;;      :empty-starting-bases #{}
-;;      :bases-being-checked #{}
-;;      :bases-checked #{}
-;;      :enemy-starting-base nil}
 
 ;; (defn possible-enemy-base? [{:keys [possible-enemy-starting-bases]}])
 
